1587c1edfdb5d9f4a523b2e5f8bb4bb6b9bb58e9
jacquarg <guillaume.jacquart@hoodbrains.com>
2017-12-01 17:25:39 +0100

    Action are forbiden for the moment ...

---
+++ README.md
@@ -0,0 +1,3 @@
+# Manage Konnectors
+
+A simple Cozy App to install or update a konnector in your cozy account.
---
+++ app.js
@@ -0,0 +1,37 @@
+'use-strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = document.querySelector('[role=application]')
+  cozy.client.init({
+    cozyURL: '//' + app.dataset.cozyDomain,
+    token: app.dataset.cozyToken,
+  })
+
+  cozyUsetracker()
+  .catch(err => console.warn('Error while initializing tracking.', err))
+  // .then(() => window.cozy.bar.init({ appName: 'Manage konnectors' }));
+
+
+  document.getElementById('update').onclick = () => {
+    displayMessage('Updating konnector')
+    work('PUT')
+  }
+
+  document.getElementById('install').onclick = () => {
+    displayMessage('Installing konnector ...')
+    work('POST')
+  }
+})
+
+function work (verb) {
+  const slug = document.getElementById('slug').value
+  const source = document.getElementById('repositoryuri').value
+  return cozy.client.fetchJSON(verb, `/konnectors/${slug}?Source=${encodeURIComponent(source)}`) //, '', { headers: { 'Accept': 'text/event-stream' } })
+  .then(() => displayMessage('done'))
+  .catch((err) => displayMessage(err))
+}
+
+function displayMessage (msg) {
+  const messageElem = document.getElementById('message')
+  messageElem.innerHTML = msg
+}
